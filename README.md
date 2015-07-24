af9cb326ad11b649ec0cb7df4eedb4f5fa3f8f43
Tom Whyntie <Tom Whyntie>
2015-07-24 22:10:13 +0100

    Committed files from Drywall plus tweaks.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# drywalltest001
-Testing out the Drywall node.js user management system.
+#Drywall Testing
+
+The original Drywall can be found [here](https://github.com/jedireza/drywall/):
+
+* https://github.com/jedireza/drywall/
+
+Tweaks have been made to enable:
+
+* Environment variable configuration via .env files;
+* MongoLab and SendGrid integration via Heroku _and_ dev (grunt).
---
+++ config.js
@@ -0,0 +1,61 @@
+'use strict';
+
+exports.port = process.env.PORT || 3000;
+
+// For retrieving (local) environment variables.
+var env = require('node-env-file');
+//
+if (!process.env.MONGOLAB_URI) {
+  console.log('process.env.MONGOLAB_URI is undefined - dev mode?');
+  // If we're in development, get the environment variables.
+  env(__dirname + '/.env');
+}
+
+exports.mongodb = {
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/drywall'
+};
+exports.companyName = 'T. Whyntie';
+exports.projectName = 'Drywall (Test)';
+exports.systemEmail = process.env.SENDGRID_USERNAME;
+exports.cryptoKey = 'k3yb0ardc4t';
+exports.loginAttempts = {
+  forIp: 50,
+  forIpAndUser: 7,
+  logExpiration: '20m'
+};
+exports.requireAccountVerification = false;
+exports.smtp = {
+  from: {
+    name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
+    address: process.env.SMTP_FROM_ADDRESS || process.env.SENDGRID_USERNAME || ''
+  },
+  credentials: {
+    user: process.env.SMTP_USERNAME || process.env.SENDGRID_USERNAME || '',
+    password: process.env.SMTP_PASSWORD || process.env.SENDGRID_PASSWORD || '',
+    host: process.env.SMTP_HOST || 'smtp.sendgrid.net' || '',
+    ssl: false
+  }
+};
+exports.oauth = {
+  twitter: {
+    key: process.env.TWITTER_OAUTH_KEY || '',
+    secret: process.env.TWITTER_OAUTH_SECRET || ''
+  },
+
+  facebook: {
+    key: process.env.FACEBOOK_OAUTH_KEY || '',
+    secret: process.env.FACEBOOK_OAUTH_SECRET || ''
+  },
+  github: {
+    key: process.env.GITHUB_OAUTH_KEY || '',
+    secret: process.env.GITHUB_OAUTH_SECRET || ''
+  },
+  google: {
+    key: process.env.GOOGLE_OAUTH_KEY || '',
+    secret: process.env.GOOGLE_OAUTH_SECRET || ''
+  },
+  tumblr: {
+    key: process.env.TUMBLR_OAUTH_KEY || '',
+    secret: process.env.TUMBLR_OAUTH_SECRET || ''
+  }
+};
